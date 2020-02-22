9b65c6ef9e9ad7020ec28782e69a2aea9815e78c
Mario Noseda <mnoseda@bluewin.ch>
2020-02-22 15:27:15 +0100

    list sum solved

--- recursion_examples.py
+++ recursion_examples.py
@@ -1,4 +1,34 @@
 import recursionex.list_sum
+import logging
+import unittest
+
+
+class ListSumTestCase(unittest.TestCase):
+	def test_sum_1(self):
+		self.assertEqual(recursionex.list_sum.sum([1,2,3]), 6)
+
+	def test_sum_2(self):
+		self.assertEqual(recursionex.list_sum.sum([11,44,1,1,1,0,0,0]), 58)
+
+	def test_sum_3(self):
+		self.assertEqual(recursionex.list_sum.sum([0,0,0]), 0)
+
+	def test_sum_empty(self):
+		self.assertEqual(recursionex.list_sum.sum([]), 0)
+
+	def test_sum_negative(self):
+		self.assertEqual(recursionex.list_sum.sum([-3, -16, -4, -50]), -73)
+
+	def test_sum_mixed(self):
+		self.assertEqual(recursionex.list_sum.sum([10,-6,-4]),0)
+
+
 
 if __name__ == "__main__":
-	recursionex.list_sum.test()
+	log_format = "%(asctime)s %(levelname)s - %(message)s"
+	logging.basicConfig(format=log_format, level=logging.DEBUG)
+	logging.debug("Starting now!")
+
+	unittest.main()
+
+	# print("Final result: {}".format(recursionex.list_sum.sum([1,2,3])))
--- recursionex/list_sum.py
+++ recursionex/list_sum.py
@@ -1,2 +1,11 @@
-def test():
-	print("testing of list_sum")
+import logging
+
+def sum(l):
+	if not l: #empty list
+		logging.debug("empty list. return 0")
+		return 0
+	else:
+		logging.debug("first elem: {}  invoking func with remaining list: {}".format(l[0], l[1:]))
+		intermediate_sum = (l[0] + sum(l[1:]))
+		logging.debug("intermediate_sum: {}".format(intermediate_sum))
+		return intermediate_sum
